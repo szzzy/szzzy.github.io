bf3ca3bbd8576d70ebf5ab9042972123c968cd02
szzzy <1925861393@qq.com>
2022-07-02 20:21:58 +0800

    Site updated: 2022-07-02 20:21:57

--- js/rdmbkg.js
+++ js/rdmbkg.js
@@ -1,9 +1,9 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(/img/bg1.jpg)",
-  "url(/img/bg2.png)",
-  "url(/img/bg3.jpg)",
-  "url(/img/bg4.png)"
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1afa.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b02.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b1a.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b21.jpg)"
 ];
 //获取背景图片总数，生成随机数
 var bgindex =Math.floor(Math.random() * (backimg.length));
@@ -11,10 +11,10 @@ var bgindex =Math.floor(Math.random() * (backimg.length));
 document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
 //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var bannerimg =[
-  "url(/img/bg1.jpg)",
-  "url(/img/bg2.png)",
-  "url(/img/bg3.jpg)",
-  "url(/img/bg4.png)"
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1afa.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b02.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b1a.jpg)",
+  "url(https://pic.imgdb.cn/item/62c037be5be16ec74a8c1b21.jpg)"
 ];
 //获取banner图片总数，生成随机数
 var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
